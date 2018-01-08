03f1bc2a83c9629a49e56bdc995b741dbfd84769
githubuttu <utkarsh.cs24@gmail.com>
2018-01-08 19:12:50 +0530

    SourceCode

---
+++ TestTravis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTravis
+//
+//  Created by ImpactQA on 05/01/18.
+//  Copyright © 2018 ImpactQA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTravis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTravis
+//
+//  Created by ImpactQA on 05/01/18.
+//  Copyright © 2018 ImpactQA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
